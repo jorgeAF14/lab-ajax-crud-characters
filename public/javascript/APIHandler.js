c0812aa17619cf868d2624495eb729e90bd01a18
Jorge Garzon <jgarzon.informatica@gmail.com>
2022-02-09 15:59:09 +0100

    paso1

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,27 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.axiosApp = axios.create(baseUrl)
   }
 
   getFullList () {
+    return this.axiosApp.get('/characters')
+  }
+
+  getOneRegister(characterId) {
+    return this.axiosApp.get(`/characters/${characterId}`)
 
   }
 
-  getOneRegister () {
-
+  createOneRegister (infoCharacter) {
+    return this.axiosApp.post('/characters', infoCharacter)
   }
 
-  createOneRegister () {
-
+  updateOneRegister(characterId, characterInfo) {
+    return this.axiosApp.put(`/characters/${characterId}`, characterInfo)
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(characterId) {
+    return this.axiosApp.post(`/characters/${characterId}`)
   }
 }
